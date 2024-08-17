e004be26f5b329766b56e6b3f5547a8af0243580
Kai Ma <k78ma@uwaterloo.ca>
2024-08-17 13:50:50 -0600

    cleaned up quicksort

--- sorting-algorithms/quicksort.py
+++ sorting-algorithms/quicksort.py
@@ -1,34 +1,23 @@
 def partition (array, low, high):
-    pivot = high; # Use last element as pivot value
+    pivot = high # Use last element as pivot
     pivot_position = low; # Track index of pivot
-
-    # print("Pivot number: ", array[pivot])
     for i in range(low, high):
 
-        # print("Index: ", i, ", Value: ", array[i])
-
-        # If the current element is less than the pivot, we swap it with the element at pivot_position and increment pivot_position
+        # If the current element is less than the last element, we swap it with the element at pivot_position and increment pivot_position
         if array[i] < array[pivot]:
-            # print("Swap!")
             array[i], array[pivot_position] = array[pivot_position], array[i] # Swap
             pivot_position += 1 # Increment the firstHighIndex
 
-        # print(array)
-        # print(pivot_position)
-        # print()
-
-    # print("Swapping pivot to correct position")
-    array[pivot], array[pivot_position] = array[pivot_position], array[pivot]
+    array[pivot], array[pivot_position] = array[pivot_position], array[pivot] # Swap the pivot from the end into the right position
     return pivot_position
 
 def quicksort (array, low, high):
-
     if low < high:
         pivot = partition(array, low, high)
         quicksort(array, low, pivot - 1)
         quicksort(array, pivot + 1, high)
 
-testArray = [9,4,1,3,5,8,6,2,7]
-quicksort(testArray, 0, 8)
+testArray = [2,5,6,7,9,3,10,1,8,4]
 
+quicksort(testArray, 0, 9)
 print(testArray)
