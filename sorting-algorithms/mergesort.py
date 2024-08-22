98bb1a20211d2ea6879a7114e24833d90227cd08
Kai Ma <k78ma@uwaterloo.ca>
2024-08-22 14:13:03 -0600

    mergesort

--- sorting-algorithms/mergesort.py
+++ sorting-algorithms/mergesort.py
@@ -1,1 +1,44 @@
+def mergesort(array, low, high):
+    if low < high:
+        middle = (low + high) // 2
+        mergesort(array, low, middle)
+        mergesort(array, middle + 1, high)
+        merge(array, low, middle, high)
 
+def merge(array, low, middle, high):
+
+    # Create temporary arrays to hold the two subarrays
+    left_subarray = array[low:middle + 1]
+    right_subarray = array[middle + 1:high + 1]
+
+    # Initial indexes for the subarrays and the main array
+    left_idx = 0  # Initial index for left subarray
+    right_idx = 0  # Initial index for right subarray
+    merged_idx = low  # Initial index for merged array
+
+    # Merge the temporary arrays back into the original array
+    while left_idx < len(left_subarray) and right_idx < len(right_subarray):
+        if left_subarray[left_idx] <= right_subarray[right_idx]:
+            array[merged_idx] = left_subarray[left_idx]
+            left_idx += 1
+        else:
+            array[merged_idx] = right_subarray[right_idx]
+            right_idx += 1
+        merged_idx += 1
+
+    # Copy any remaining elements of left_subarray, if any
+    while left_idx < len(left_subarray):
+        array[merged_idx] = left_subarray[left_idx]
+        left_idx += 1
+        merged_idx += 1
+
+    # Copy any remaining elements of right_subarray, if any
+    while right_idx < len(right_subarray):
+        array[merged_idx] = right_subarray[right_idx]
+        right_idx += 1
+        merged_idx += 1
+
+testArray = [2,5,6,7,9,3,10,1,8,4]
+
+mergesort(testArray, 0, 9)
+print(testArray)
--- sorting-algorithms/quicksort.py
+++ sorting-algorithms/quicksort.py
@@ -1,3 +1,10 @@
+def quicksort (array, low, high):
+    if low < high:
+        pivot = partition(array, low, high)
+        quicksort(array, low, pivot - 1)
+        quicksort(array, pivot + 1, high)
+
+
 def partition (array, low, high):
     pivot = high # Use last element as pivot
     pivot_position = low; # Track index of pivot
@@ -11,11 +18,6 @@ def partition (array, low, high):
     array[pivot], array[pivot_position] = array[pivot_position], array[pivot] # Swap the pivot from the end into the right position
     return pivot_position
 
-def quicksort (array, low, high):
-    if low < high:
-        pivot = partition(array, low, high)
-        quicksort(array, low, pivot - 1)
-        quicksort(array, pivot + 1, high)
 
 testArray = [2,5,6,7,9,3,10,1,8,4]
 
